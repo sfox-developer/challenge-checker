24c461603e4790fe19809ab4bc1533f0d05f6dd7
sfox-developer <k.weibrecht@simplefox.de>
2025-12-13 19:45:16 +0100

    fix: fixed profile includes

--- resources/views/dashboard/profile/edit.blade.php
+++ resources/views/dashboard/profile/edit.blade.php
@@ -14,19 +14,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="card p-4 sm:p-8">
                 <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+                    @include('dashboard.profile.partials.update-profile-information-form')
                 </div>
             </div>
 
             <div class="card p-4 sm:p-8">
                 <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+                    @include('dashboard.profile.partials.update-password-form')
                 </div>
             </div>
 
             <div class="card p-4 sm:p-8">
                 <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                    @include('dashboard.profile.partials.delete-user-form')
                 </div>
             </div>
         </div>
